7a2070c45b558808fde4b18db0add9eb14bf6d79
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-14 15:31:18 +0300

    Empty key and invalid key testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,7 +6,12 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
 
+  def self.api_key
+    '2e7bbc6a71b9fcc400ac3165b35e3e1e' # replace with YOUR Tmdb key
+  end
+
   def self.find_in_tmdb(string)
+    Tmdb.api_key = self.api_key
     begin
       TmdbMovie.find(:title => string)
     rescue ArgumentError => tmdb_error
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,6 +8,12 @@ describe Movie do
       Movie.find_in_tmdb('Inception')
     end
     it 'should raise an InvalidKeyError with no API key' do
+      Movie.stub(:api_key).and_return('')
+      lambda { Movie.find_in_tmdb('Inception') }.
+          should raise_error(Movie::InvalidKeyError)
+    end
+    it 'should raise an InvalidKeyError with invalid API key' do
+      Movie.stub(:api_key).and_return('INVALID')
       lambda { Movie.find_in_tmdb('Inception') }.
           should raise_error(Movie::InvalidKeyError)
     end
