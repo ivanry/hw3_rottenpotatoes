955ea588fb36f4bcfc5c0101ca152d53f68fb938
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-14 15:02:10 +0300

    InvalidKeyError added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,17 @@
 class Movie < ActiveRecord::Base
+
+  class Movie::InvalidKeyError < StandardError ; end
+
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
 
   def self.find_in_tmdb(string)
-    TmdbMovie.find(:title => string)
+    begin
+      TmdbMovie.find(:title => string)
+    rescue ArgumentError => tmdb_error
+      raise Movie::InvalidKeyError, tmdb_error.message
+    end
   end
 
 end
