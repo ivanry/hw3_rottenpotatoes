dd3c2d512bde921d0b1b657b4a1f3cbe3129f99e
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-14 14:50:40 +0300

    more testing

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Movie do
   describe 'searching Tmdb by keyword' do
-    it 'should call Tmdb with title keywords' do
-      TmdbMovie.should_receive(:find).with(hash_including :title => 'Inception')
+    it 'should call Tmdb with title keywords given valid API key' do
+      TmdbMovie.should_receive(:find).
+          with(hash_including :title => 'Inception')
       Movie.find_in_tmdb('Inception')
+    end
+    it 'should raise an InvalidKeyError with no API key' do
+      lambda { Movie.find_in_tmdb('Inception') }.
+          should raise_error(Movie::InvalidKeyError)
     end
   end
 end
