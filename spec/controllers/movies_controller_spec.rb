9128474a3d040950b30f364e8af49c06b6b77548
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-14 12:45:42 +0300

    Fixture added Test which use fixture added

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,8 +3,21 @@ require 'ruby-debug'
 
 describe MoviesController do
   describe 'searching TMDb' do
+    fixtures :movies
     before :each do
       @fake_results = [mock('movie1'), mock('movie2')]
+      @fake_movie = mock('Movie')
+      @fake_movie.stub(:title).and_return('Casablanca')
+      @fake_movie.stub(:rating).and_return('PG')
+      @fake_movie.stub(:name_with_rating).and_return('Casablanca (PG)')
+    end
+    it 'makes my test2' do
+      movie = movies(:milk_movie)
+      movie.title.should == 'Milk1'
+      movie.rating.should == 'R'
+    end
+    it 'makes my test' do
+      @fake_movie.name_with_rating.should == 'Casablanca (PG)'
     end
     it 'should call the model method that performs TMDb search' do
       Movie.should_receive(:find_in_tmdb).with('hardware').
